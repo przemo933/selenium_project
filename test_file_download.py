d7f816d60e1459cfa2fb0f1bf8bea11b13cf2135
karolinaperdek <perdekkarolina@gmail.com>
2023-06-04 14:15:05 +0200

    finish TestFileDownload (04.06.23) - Karolina

--- test_file_download.py
+++ test_file_download.py
@@ -1,8 +1,6 @@
-import requests as requests
-
 from test_case import TestCase
 from selenium.webdriver.common.by import By
-import os
+from selenium.webdriver.common.keys import Keys
 import time
 
 
@@ -12,13 +10,25 @@ class TestFileDownload(TestCase):
 
         #pobieranie przykladowego pliku ze strony
         try:
-            gotit = self.driver.find_element(By.CSS_SELECTOR, '.example > a:nth-child(26)')
-            gotit.click()
+            self.driver.find_element(By.CSS_SELECTOR, '.example > a:nth-child(26)').click()
             time.sleep(3)
         except Exception:
             self.fail("File's name in not found")
 
-    def test_file_download_2(self):
-        self.driver.get("http://the-internet.herokuapp.com/download")
+    def test_file_generate_and_download(self):
+        self.driver.get("https://demo.automationtesting.in/FileDownload.html")
+        self.driver.maximize_window()
 
-        #pobieranie przykladowego pliku ze strony
+        #generowanie i pobieranie pliku txt
+        self.driver.find_element(By.XPATH, '//*[@id="textbox"]').send_keys('testing download text file')
+        self.driver.find_element(By.XPATH, '//*[@id="createTxt"]').send_keys(Keys.ENTER)
+        self.driver.find_element(By.XPATH, '//*[@id="link-to-download"]').send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # #gererowanie i pobieranie pliku pdf
+        self.driver.find_element(By.ID, 'pdfbox').send_keys('testing pdf')
+        self.driver.find_element(By.ID, 'createPdf').send_keys(Keys.ENTER)
+        self.driver.find_element(By.ID, 'pdf-link-to-download').send_keys(Keys.ENTER)
+        time.sleep(1)
+
+
