32d5d9acc0f5eb846d37e9d927d1f7cbcc661794
Przemysław Sobieraj <przemo.sob@gmail.com>
2023-06-04 15:00:58 +0200

    finish TestUpload (04.06) - Przemek

--- test_file_upload.py
+++ test_file_upload.py
@@ -1,18 +1,17 @@
 from selenium.webdriver.common.by import By
 from test_case import TestCase
 
+
 class TestFileUpload(TestCase):
 
     # funkcja testujaca upload pliku
     def test_file_upload(self):
         self.driver.get("http://the-internet.herokuapp.com/upload")
         self.driver.find_element(By.ID, "file-upload").send_keys("/Users/MacBookPro/Git_repos/selenium_project/"
-                                                                     "test_files/test.txt")
+                                                                 "test_files/test.txt")
         self.driver.find_element(By.ID, "file-submit").submit()
 
         if self.driver.page_source.find("File Uploaded!"):
             print("file upload success")
         else:
             print("file upload not successful")
-
-
